4f7b3104dcc7b1f8d887ab5d18ac0b594b5ef691
Sara Hänzi <sara.programs@proton.me>
2026-02-01 20:29:28 +0100

    correct old zurich data, only to previous year

--- data-raw/01_get_data.R
+++ data-raw/01_get_data.R
@@ -14,7 +14,7 @@ get_winti_data_to_2021 <- function() {
 }
 
 get_complete_zh_data <- function() {
-  zh_files <- purrr::map(c(2019:lubridate::year(lubridate::now())),
+  zh_files <- purrr::map(c(2019:(lubridate::year(lubridate::now())-1)),
   \(x) glue::glue("https://data.stadt-zuerich.ch/dataset/ewz_bruttolastgang_stadt_zuerich/download/",
                x, "_ewz_bruttolastgang.csv"))
     
